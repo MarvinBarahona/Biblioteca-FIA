d89348004d00f791309be3d8754105fe2e57c816
Marvin Barahona <marosvramirez@gmail.com>
2017-08-29 23:43:40 -0600

    Agregando versión inicial del menú

--- BibliotecaUES-Admin/src/app/libros/componentes/libros-root.component.ts
+++ BibliotecaUES-Admin/src/app/libros/componentes/libros-root.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'libros-root',
-  templateUrl: './libros-root.component.html'
+  templateUrl: './libros-root.component.html',
+  styleUrls: ['./libros-root.component.css']
 })
 export class LibrosRootComponent implements OnInit {
 
