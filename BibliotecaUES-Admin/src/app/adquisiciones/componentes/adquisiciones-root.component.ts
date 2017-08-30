f774b0862968a4a472d38df27a63032d59ea2db7
Marvin Barahona <marosvramirez@gmail.com>
2017-08-30 07:16:34 -0600

    Agregando el nav-side a los módulos

--- BibliotecaUES-Admin/src/app/adquisiciones/componentes/adquisiciones-root.component.ts
+++ BibliotecaUES-Admin/src/app/adquisiciones/componentes/adquisiciones-root.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'adquisiciones-root',
-  templateUrl: './adquisiciones-root.component.html'
+  templateUrl: './adquisiciones-root.component.html',
+  styleUrls: ['./adquisiciones-root.component.css']
 })
 export class AdquisicionesRootComponent implements OnInit {
 
--- BibliotecaUES-Admin/src/app/ejemplares/componentes/ejemplares-root.component.ts
+++ BibliotecaUES-Admin/src/app/ejemplares/componentes/ejemplares-root.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ejemplares-root',
-  templateUrl: './ejemplares-root.component.html'
+  templateUrl: './ejemplares-root.component.html',
+  styleUrls: ['./ejemplares-root.component.css']
 })
 export class EjemplaresRootComponent implements OnInit {
 
