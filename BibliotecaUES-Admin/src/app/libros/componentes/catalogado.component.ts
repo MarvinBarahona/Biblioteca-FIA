785dbd00e9e451d45a5e2370b53ac4e0a09ab0fe
Esmeralda <lg.esme@hotmail.com>
2017-09-10 01:01:59 -0600

    Estructura HTML base completa

--- BibliotecaUES-Admin/src/app/libros/componentes/catalogado.component.ts
+++ BibliotecaUES-Admin/src/app/libros/componentes/catalogado.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'catalogado',
-  templateUrl: './catalogado.component.html'
+  templateUrl: './catalogado.component.html',
+  styles: [`
+      .previa {
+        width: 150px;
+        height: 206px;
+        margin-top:5px;
+      }
+      `]
 })
 export class CatalogadoComponent implements OnInit {
 
