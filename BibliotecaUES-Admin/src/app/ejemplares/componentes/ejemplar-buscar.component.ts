a092874e68fc8112b0823a6b6b8a6d5f7437dc2d
Esmeralda <lg.esme@hotmail.com>
2017-09-13 02:35:03 -0600

    Agregada la búsqueda de ejemplares

--- BibliotecaUES-Admin/src/app/ejemplares/componentes/ejemplar-buscar.component.ts
+++ BibliotecaUES-Admin/src/app/ejemplares/componentes/ejemplar-buscar.component.ts
@@ -5,6 +5,11 @@
 **/
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { EjemplaresService, Ejemplar } from './../servicios';
+
+declare var $:any;
 
 @Component({
   templateUrl: './ejemplar-buscar.component.html',
@@ -16,10 +21,64 @@ import { Component, OnInit } from '@angular/core';
       `]
 })
 export class EjemplarBuscarComponent implements OnInit {
+  ejemplares: Ejemplar[];
+  codigos = Array<string>();
+  ejemplar: Ejemplar;
+  codigo: string;
+  error: string = "No se encontraron resultados";
 
-  constructor() { }
+  constructor(private ejemplaresService: EjemplaresService, private router: Router) { }
 
   ngOnInit() {
+    this.inicializarAutocompletado();
   }
 
+  // Inicializar input con opciones de autocompletar
+  inicializarAutocompletado(){
+    this.ejemplaresService.obtenerTodos().subscribe(
+      ejemplares => {
+        this.ejemplares = ejemplares;
+
+        var i=0;
+        for(var i:number; i<this.ejemplares.length; i++){
+          if(ejemplares[i].codigo != null){
+            this.codigos.push(ejemplares[i].codigo);
+          }
+        }
+
+        // Transforma los códigos en un objeto para el autocompletado
+        let codigosData = {};
+        this.codigos.forEach(function(codigo){
+          codigosData[codigo] = null;
+        });
+
+        // Inicializar el campo con autocompletado
+        $('#codigo').autocomplete({
+          data: codigosData,
+          limit: 5,
+          minLength: 1
+        });
+      }
+    );
+  }
+
+  //Buscar un libro por medio del código de barra
+  buscar(form:any){
+    let codigo = $("#codigo").val();
+    this.ejemplaresService.obtenerPorCodigo(codigo).subscribe(
+      ejemplar => {
+        this.ejemplar = ejemplar;
+      }
+    )
+  }
+
+  // Redirigir a la vista de un libro
+  linkLibro(id: number){
+    this.router.navigate(['/libros/'+id]);
+  }
+
+  // Redirigir a la vista de un ejemplar
+  linkEjemplar(id: number){
+    this.router.navigate(['/ejemplares/'+id]);
+  }
 }
