ac5b2be1d4a0ae40d772fc9c70e0ccee3666c9f4
Marvin Barahona <marosvramirez@gmail.com>
2017-10-01 00:34:55 -0600

    Agregando nueva sugerencia para docente y buscador

--- BibliotecaUES-Buzon/src/app/sugerencias/componentes/sugerencia-nueva-docente.component.ts
+++ BibliotecaUES-Buzon/src/app/sugerencias/componentes/sugerencia-nueva-docente.component.ts
@@ -3,11 +3,11 @@
 *Dirección física: src\app\sugerencias\componentes\sugerencia-nueva-docente.component.ts
 *Objetivo: Crear sugerencia de un docente
 **/
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MaterializeDirective, MaterializeAction } from "angular2-materialize";
 
-import { SugerenciasService } from './../servicios'
+import { SugerenciasService, Carrera, Sugerencia } from './../servicios'
 
 declare var Materialize: any;
 declare var $: any;
@@ -16,24 +16,93 @@ declare var $: any;
   templateUrl: './sugerencia-nueva-docente.component.html'
 })
 export class SugerenciaNuevaDocenteComponent implements OnInit {
+  carreras: Carrera[];
+  carreraSelect: Carrera;
+  sugerencia: Sugerencia;
+  idMateria: number;
 
+  showMessage: boolean;
+  errorMessage: string;
+  showFailMessage: boolean;
+  linkId: string;
 
-constructor(private sugerenciasService: SugerenciasService, private route: ActivatedRoute, private router: Router){}
+  constructor(
+    private sugerenciasService: SugerenciasService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private cd: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
-    this.inicializarAutocompletado();
+    this.sugerencia = new Sugerencia;
+    this.showFailMessage = false;
+    this.showMessage = false;
+
+    this.sugerenciasService.obtenerCarreras().subscribe(
+      carreras => {
+        this.carreras = carreras;
+        // Para actualizar la vista del select
+        this.cd.detectChanges();
+        this.carreraSelect = this.carreras[0];
+        this.inicializarAutocompletado();
+      }
+    );
   }
 
-  crear(){}
+  crear() {
+    this.showMessage = true;
+    this.errorMessage = null;
+    this.showFailMessage = false;
+
+    this.sugerenciasService.crear(this.sugerencia, this.idMateria, false).subscribe(
+      (r) => {
+        if(r['saved']) this.router.navigate(['/sugerencias']);
+        else{
+          this.showFailMessage = true;
+          this.showMessage = false;
+          this.linkId = r['suggestionId'];
+        }
+      },
+      (error) => {
+        let r = error.json();
+        let errors = r['errors'];
+        this.showFailMessage = true;
+        this.showMessage = false;
+        this.linkId = errors['suggestionId'];
+      }
+    );
+  }
+
+  // Link a sugerencia
+  linkSugerencia(){
+    this.router.navigate(['/sugerencias/votar/'+this.linkId]);
+  }
+
+  // Al seleccionar una carreras
+  selectCarrera(val: string) {
+    this.carreras.forEach((carrera) => {
+      if (carrera.nombre == val) {
+        this.carreraSelect = carrera
+        this.inicializarAutocompletado();
+      }
+    });
+  }
 
   // Inicializar el campo con autocompletado.
-  inicializarAutocompletado(){
-    $('#materias').material_chip({
-      autocompleteOptions: {
-        data: {
-        },
-        limit: 5,
-        minLength: 1
+  inicializarAutocompletado() {
+    let data = {};
+    this.carreraSelect.materias.forEach((materia) => {
+      data[materia.codigo + " - " + materia.nombre] = null;
+    });
+
+    $('#materia').autocomplete({
+      data: data,
+      limit: 5,
+      minLength: 1,
+      onAutocomplete: (val: string) => {
+        this.carreraSelect.materias.forEach((materia) => {
+          if (val.startsWith(materia.codigo)) this.idMateria = materia.id;
+        });
       }
     });
   }
--- BibliotecaUES-Buzon/src/app/sugerencias/componentes/sugerencias.component.ts
+++ BibliotecaUES-Buzon/src/app/sugerencias/componentes/sugerencias.component.ts
@@ -14,10 +14,13 @@ import { SugerenciasService, Sugerencia } from './../servicios';
 })
 export class SugerenciasComponent implements OnInit {
   sugerencias: Sugerencia[];
+  textoBusqueda: string;
+  criterioBusqueda: string;
 
   constructor(private sugerenciasService: SugerenciasService, private router: Router) {}
 
   ngOnInit(): void {
+    this.criterioBusqueda = '0';
     this.sugerenciasService.top10().subscribe(
       sugerencias => {
         this.sugerencias = sugerencias;
@@ -26,6 +29,30 @@ export class SugerenciasComponent implements OnInit {
 
   }
 
-  buscar(){}
+  buscar(){
+    this.sugerencias = null;
+    let key;
+    switch(this.criterioBusqueda){
+      case '0': {
+        key = 'title';
+        break;
+      }
+      case '1': {
+        key = 'isbn';
+        break;
+      }
+      case '2': {
+        key = 'subjectCode';
+        break;
+      }
+      default:{}
+    }
+
+    this.sugerenciasService.buscar(key, this.textoBusqueda).subscribe(
+      sugerencias => {
+        this.sugerencias = sugerencias;
+      }
+    );
+  }
 
 }
